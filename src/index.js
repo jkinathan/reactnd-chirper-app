b69e6d22452cd081fec2312694e705ae439dedf8
jkinathan <jkinobe@gmail.com>
2021-08-26 10:24:51 +0300

    feat: updating npm and adding react-redux and redux to the application

--- src/index.js
+++ src/index.js
@@ -3,4 +3,6 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './components/App'
 
+import { createstore } from redux
+
 ReactDOM.render(<App />, document.getElementById('root'))
