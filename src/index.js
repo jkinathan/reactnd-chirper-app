c3de3af2bf52b229b66b7e7e3133395d0ad37da5
jkinathan <jkinobe@gmail.com>
2021-08-26 11:42:52 +0300

    feat: creating my custom logger middleware and applymiddleware along with thunk from redux-thunk

--- src/index.js
+++ src/index.js
@@ -6,9 +6,10 @@ import App from './components/App'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import  reducer  from './Reducers/index';
+import middleware from './middleware/index'
 
 
-const store = createStore(reducer)
+const store = createStore(reducer, middleware)
 
 ReactDOM.render(
         <Provider store= { store }>
---
+++ src/middleware/logger.js
@@ -0,0 +1,13 @@
+const logger = (store) => (next) => (action) => {
+
+    console.group(action.type)
+        console.log("action: ",action)
+        const returnValue = next(action)
+        console.log("The new State: ",store.getState())
+    console.groupEnd()
+
+    return returnValue
+   
+}
+
+export default logger
