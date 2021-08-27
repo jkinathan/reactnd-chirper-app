0da8096a58fd084b0b24c3391bd0776c7344ab0a
jkinathan <jkinobe@gmail.com>
2021-08-27 12:49:58 +0300

    feat: map loading to true

--- src/components/App.js
+++ src/components/App.js
@@ -12,10 +12,20 @@ class App extends Component {
   render() {
     return (
       <div>
-        <Dashboard/>
+        {
+          this.props.loading === true ? null : <Dashboard/>
+        }
+        
       </div>
     )
   }
 }
+
+function mapStateToProps({authedUser}){
+  return{
+    loading: authedUser == null
+  }
+}
+
 
 export default connect()(App)
