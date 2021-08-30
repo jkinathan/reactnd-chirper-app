8caf0571c5a003b0e0d6993736f82f7823223dbc
jkinathan <jkinobe@gmail.com>
2021-08-30 13:11:05 +0300

    feat: Adding loading bar inside project

--- src/Actions/shared.js
+++ src/Actions/shared.js
@@ -2,17 +2,21 @@ import { getInitialData } from '../utils/api'
 import { receiveUsers } from './users'
 import { receiveTweets } from './tweets'
 import { setAuthedUser } from './authedUser'
+import { showLoading, hideLoading } from 'react-redux-loading'
 
 const AUTHEDID = 'tylermcginnis'
 
 export function handleInitialData(){
     return (dispatch)=>{
+        dispatch(showLoading())
         return getInitialData()
         .then(({ users, tweets })=>{
 
             dispatch(receiveUsers(users))
             dispatch(receiveTweets(tweets))
             dispatch(setAuthedUser(AUTHEDID))
+
+            dispatch(hideLoading())
         })
     }
 }
--- src/Reducers/index.js
+++ src/Reducers/index.js
@@ -2,10 +2,11 @@ import { combineReducers } from 'redux'
 import authedUser from '../Reducers/authedUser'
 import tweets from '../Reducers/tweets'
 import users from '../Reducers/users'
-
+import { loadingBarReducer } from 'react-redux-loading'
 
 export default combineReducers({
     authedUser: authedUser,
     tweets:tweets,
-    users:users
+    users:users,
+    loadingBar:loadingBarReducer
 })
